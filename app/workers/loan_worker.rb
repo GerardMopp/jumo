be2d567c811edd032b171019576844871d8b6dfc
Gerard Mopp <gerardmopp@gmail.com>
2017-05-14 15:08:49 +0200

    adding pusher

--- app/workers/loan_worker.rb
+++ app/workers/loan_worker.rb
@@ -2,34 +2,41 @@ class LoanWorker
   include Sidekiq::Worker
 
   def perform(*args)
-  	loan = Loan.find_by_id(args[0])
-  	hash = {}
-  	SmarterCSV.process(loan.input.path, {}) do |row|
-  		network = format_header(row[0][:network])
-  		month = Date.parse(format_header(row[0][:date])).strftime('%B')
-  		product = format_header(row[0][:product])
-  		hash["#{network} - #{product} - #{month}"]  = 0 unless hash.has_key?("#{network} - #{product} - #{month}")
-  		hash["#{network} - #{product} - #{month}"] += row[0][:amount].to_f 
-		end
+    loan = Loan.find_by_id(args[0])
+    begin
+      hash = {}
+      SmarterCSV.process(loan.input.path, {}) do |row|
+        network = format_header(row[0][:network])
+        month = Date.parse(format_header(row[0][:date])).strftime('%B')
+        product = format_header(row[0][:product])
+        hash["#{network} - #{product} - #{month}"]  = 0 unless hash.has_key?("#{network} - #{product} - #{month}")
+        hash["#{network} - #{product} - #{month}"] += row[0][:amount].to_f 
+      end
 
-    full_filename = "#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S").downcase}/Output.csv"
-    Dir.mkdir( "#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S").downcase}") unless File.exists?("#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S").downcase}")
-    CSV.open(full_filename, "wb") do |csv|
-      headers = ['Network', 'Product', 'Month', 'Aggregate']
-      csv << headers
-      hash.each do |key, value|
-      	csv << key.split(' - ') + [value]
-			end
+      full_filename = "#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S").downcase}/Output.csv"
+      Dir.mkdir( "#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S").downcase}") unless File.exists?("#{Rails.root}/tmp/#{Time.now.strftime("%Y%m%d%H%M%S").downcase}")
+      CSV.open(full_filename, "wb") do |csv|
+        headers = ['Network', 'Product', 'Month', 'Aggregate']
+        csv << headers
+        hash.each do |key, value|
+          csv << key.split(' - ') + [value]
+        end
+      end
+
+      File.open(full_filename) do |f|
+        loan.output = f
+      end
+      
+      loan.save!
+      Pusher["loan_#{loan.id}"].trigger('done', {
+        :done => true
+      })
+    rescue Exception => e
+      Pusher["loan_#{loan.id}"].trigger('error', {
+        :error => e
+      })
+    raise e 
     end
-
-    File.open(full_filename) do |f|
-		  loan.output = f
-		end
-		
-    loan.save!
-
-
-    # Notifier.export_easy_reg_users(full_filename, filename, email).deliver
   end
 
   def format_header(value)
